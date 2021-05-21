6ad789c272a46eae44ce2a63458497534aaa3e73
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-05-21 17:16:52 +0900

    posts_create実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,14 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.crete!(post_params)
+    redirect_to action: :index
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :material, :recipe, :video, :image)
+  end
 end
