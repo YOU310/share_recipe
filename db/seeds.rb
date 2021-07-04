d8ea792d1c49c62eded3c30c01f9164b0721520f
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-07-04 15:49:35 +0900

    admin login

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
-user1 = User.find_or_create_by!(name: "test1", email: "test1@example.com", password: "password1")
-user2 = User.find_or_create_by!(name: "test2", email: "test2@example.com", password: "password2")
-user3 = User.find_or_create_by!(name: "test3", email: "test3@example.com", password: "password3")
+email = 'test@example.com'
+password = 'password'
 
-post1 = Post.find_or_create_by!(title: "Hello", user_id: user2.id)
-post2 = Post.find_or_create_by!(title: "Hi", user_id: user3.id)
-post3 = Post.find_or_create_by!(title: "Bye", user_id: user2.id)
-
-Comment.find_or_create_by!(content: "comment1", post_id: post3.id, user_id: user3.id)
-Comment.find_or_create_by!(content: "comment2", post_id: post1.id, user_id: user1.id)
-Comment.find_or_create_by!(content: "comment3", post_id: post2.id, user_id: user3.id)
-AdminUser.find_or_create_by!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
+# テストユーザーが存在しないときだけ作成
+User.find_or_create_by!(email: email) do |user|
+  user.password = password
+  puts 'ユーザーの初期データインポートに成功しました。'
+end
+AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
