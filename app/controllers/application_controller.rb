722359a40474ac923faf07ffbfed11e2cf363ece
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-07-04 14:51:53 +0900

    devise:name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parametars, if: devise_controller?
+  before_action :configure_permitted_parametars, if: :devise_controller?
+end
+
+def configure_permitted_parametars
+  added_attrs = [:email, :name, :password, :password_comfirmation]
+  devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
 end
