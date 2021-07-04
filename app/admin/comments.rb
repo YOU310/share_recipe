7d8ae7d71b9d018ea56c066b034239b74c27eb42
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-07-04 16:46:11 +0900

    日本語化

---
+++ app/admin/comments.rb
@@ -0,0 +1,19 @@
+ActiveAdmin.register Comment do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :post_id, :content, :image, :title
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :post_id, :content, :image, :title]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+  permit_params :user, :post, :content, :image
+end
