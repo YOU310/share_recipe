cc5f18324376645cdbe800fd58576c87b2dc913c
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-07-09 15:15:39 +0900

    max_width設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def max_width
+    if controller_name == "comments" && action_name == "show"
+      "mw-md"
+    elsif devise_controller?
+      "mw-sm"
+    else
+      "mw-xl"
+    end
+  end
 end
