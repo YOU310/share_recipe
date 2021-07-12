c62aeb9353f4f39e06f42c0887d3737ca68b19cd
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-07-12 16:13:55 +0900

    管理者画面を編集

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -14,14 +14,53 @@ ActiveAdmin.register Post do
   #   permitted
   # end
 
+  permit_params :name, :video, :image, :title, :material, :recipe, :genre
+
   index do
-    column :id
+    selectable_column
+    id_column
     column :title
+    column :genre, &:genre_i18n
     column :user
     column :created_at
     column :updated_at
     actions
   end
 
-  permit_params :name, :video, :image, :title, :material, :recipe
+  show do
+    attributes_table do
+      row :id
+      row :title
+      row :genre, &:genre_i18n
+      row :user
+      row :material
+      row :recipe
+      row :image
+      row :video
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      input :user
+      input :title
+      input :genre, as: :select, collection: Post.genres_i18n.invert
+      input :material
+      input :recipe
+      input :image, as: :file
+      input :video, as: :file
+    end
+    f.actions
+  end
+
+  filter :genre, as: :select, collection: Post.genres_i18n.invert.transform_values {|v| Post.genres[v] }
+  filter :user
+  filter :title
+
 end
+
+
