c984e18a882b33b9d51aa0ff45bc1c46c840e789
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-07-18 15:52:01 +0900

    rubocop警告の対応

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,11 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :ensure_normal_user, only: :destroy
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :ensure_normal_user, only: :destroy
 
-  def ensure_normal_user
-    redirect_to root_path, alert: "ゲストユーザーは削除できません。" if resource.email == "guest@example.com"
+    def ensure_normal_user
+      redirect_to root_path, alert: "ゲストユーザーは削除できません。" if resource.email == "guest@example.com"
+    end
+
+    def destroy; end
   end
 end
