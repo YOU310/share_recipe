911619c63e1c8fdad5cb3480f9f48618a16af845
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-07-31 17:58:25 +0900

    検索フォームの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,8 @@ class PostsController < ApplicationController
   PER_PAGE = 12
 
   def index
-    @posts = Post.genre_classification(params[:genre]).page(params[:page]).per(PER_PAGE)
+    @q = Post.ransack(params[:q])
+    @posts = @q.result.genre_classification(params[:genre]).page(params[:page]).per(PER_PAGE)
   end
 
   def new
