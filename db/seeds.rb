8dc911f0e076d2d01ebc7644e1d48cd49fe54177
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-09-10 14:55:00 +0900

    seed.rbの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
+name = "test"
 email = "test@example.com"
 password = "password"
 
 # テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: email) do |user|
+  user.name = name
   user.password = password
   Rails.logger.debug "ユーザーの初期データインポートに成功しました。"
 end
-AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
+AdminUser.find_or_create_by!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
