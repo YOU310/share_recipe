16e01a3c7a4a68733c5c5452e9245909b09b268c
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-09-10 13:58:07 +0900

    rubocop修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :top
 
-  def top; 
-  end
+  def top; end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,9 +2,10 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :ensure_normal_user, only: :destroy
 
+    def destroy; end
+
     def ensure_normal_user
       redirect_to root_path, alert: "ゲストユーザーは削除できません。" if resource.email == "guest@example.com"
     end
-
   end
 end
