16bf79ffb92277a5ac1181a03d60b9febb683686
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-09-10 15:03:51 +0900

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,7 @@ email = "test@example.com"
 password = "password"
 
 # テストユーザーが存在しないときだけ作成
-User.find_or_create_by!(email: email) do |user|
-  user.name = name
+User.find_or_create_by!(name: name, email: email) do |user|
   user.password = password
   Rails.logger.debug "ユーザーの初期データインポートに成功しました。"
 end
