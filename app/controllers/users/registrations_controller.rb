53e36411bd3bb1f4683e965109815f982dc32e1d
YOU310 <75014820+YOU310@users.noreply.github.com>
2021-10-01 15:46:26 +0900

    rubocop

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,8 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :ensure_normal_user, only: :destroy
 
+    def destroy; end
+
     def ensure_normal_user
       redirect_to posts_path, alert: "ゲストユーザーは削除できません。" if resource.email == "guest@example.com"
     end
