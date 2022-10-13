6fc7a0edc07813ffa50dcec6c0f3700441856332
YOU310 <75014820+YOU310@users.noreply.github.com>
2022-10-13 23:09:59 +0900

    管理者設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   user.password = password
 #   Rails.logger.debug "ユーザーの初期データインポートに成功しました。"
 # end
-AdminUser.find_or_create_by!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
+AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
 
 User.create!(
   name: "test",
